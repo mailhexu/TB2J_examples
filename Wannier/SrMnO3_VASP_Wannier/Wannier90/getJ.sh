894cb66956d29c20c56317cdfeefa0dea8fa5898
hexu <mailhexu@gmail.com>
2021-03-17 15:43:03 +0100

    update FeO example

--- Wannier/SrMnO3_VASP_Wannier/Wannier90/getJ.sh
+++ Wannier/SrMnO3_VASP_Wannier/Wannier90/getJ.sh
@@ -1,1 +1,1 @@
-wann2J.py --efermi 4.327 --emax 0.05 --elements Mn --kmesh 7 7 7 --nz 100 --rcut 10
+wann2J.py --efermi 4.327 --emax 0.05 --elements Mn --kmesh 7 7 7 --nz 100 --rcut 10 --np 1
--- Wannier/bccFe_QE_Wannier90/W90/getJ.sh
+++ Wannier/bccFe_QE_Wannier90/W90/getJ.sh
@@ -1,3 +1,3 @@
 # Note: use .pwi or .in in the filename so ase can detect it is input of QE
-wann2J.py --posfile Fe.scf.pwi --prefix_up=Fe_up --prefix_down=Fe_down --rcut 15 --kmesh 15 15 15 --elements=Fe --efermi=12.6256 --emin -12 --emax 0.00 
+wann2J.py --posfile Fe.scf.pwi --prefix_up=Fe_up --prefix_down=Fe_down --rcut 15 --kmesh 15 15 15 --elements=Fe --efermi=12.6256 --emin -12 --emax 0.00 --np 3
 
